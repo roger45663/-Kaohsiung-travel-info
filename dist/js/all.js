3a34f4d34d2f272946e960d9e8a38c3108a3bd2e
Pei-Ting Lin <roger45663@gmail.com>
2019-01-04 23:17:41 +0800

    RWD optimization

--- dist/js/all.js
+++ dist/js/all.js
@@ -17,7 +17,6 @@
     const modal = document.querySelector('#modal_bg');
     const hotArea = document.querySelector('.btn_group');
     const title = document.querySelector('.area_title');
-    const pagination = document.querySelector('.pagination');
 
     // 高雄地區資料
     const areaName = ['楠梓區', '左營區', '鼓山區', '三民區', '苓雅區', '新興區', '前金區', '鹽埕區', '前鎮區', '旗津區', '小港區', '鳳山區', '茂林區', '甲仙區', '六龜區', '杉林區', '美濃區', '內門區', '仁武區', '田寮區', '旗山區', '梓官區', '阿蓮區', '湖內區', '岡山區', '茄萣區', '路竹區', '鳥松區', '永安區', '燕巢區', '大樹區', '大寮區', '林園區', '彌陀區', '橋頭區', '大社區', '那瑪夏區', '桃源區'];
@@ -205,9 +204,6 @@
             map.setCenter(marker.getPosition());
         });
     };
-
-    
-    
 
     // 事件監聽
     areaSelect.addEventListener('change', changeArea);
