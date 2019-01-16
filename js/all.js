987294f6132718e0633ea004b8ab6b7f30777fa1
Pei-Ting Lin <roger45663@gmail.com>
2019-01-17 00:24:59 +0800

    Updates

--- js/all.js
+++ js/all.js
@@ -23,6 +23,7 @@
     const modal = document.querySelector('#modal_bg');
     const hotArea = document.querySelector('.btn_group');
     const title = document.querySelector('.area_title');
+    const pageRendering = document.querySelector('.pagination ul');
 
     // 高雄地區資料
     const areaName = ['楠梓區', '左營區', '鼓山區', '三民區', '苓雅區', '新興區', '前金區', '鹽埕區', '前鎮區', '旗津區', '小港區', '鳳山區', '茂林區', '甲仙區', '六龜區', '杉林區', '美濃區', '內門區', '仁武區', '田寮區', '旗山區', '梓官區', '阿蓮區', '湖內區', '岡山區', '茄萣區', '路竹區', '鳥松區', '永安區', '燕巢區', '大樹區', '大寮區', '林園區', '彌陀區', '橋頭區', '大社區', '那瑪夏區', '桃源區'];
@@ -78,6 +79,7 @@
         }
         
         rendering();
+        // pagination();
     }
 
     // 頁面渲染
@@ -147,7 +149,6 @@
             `;
             showData.appendChild(placeholder);
         }
-
     }
 
     // 選單比對資料進行頁面渲染
@@ -278,11 +279,43 @@
             }
         }
     }
+    
+
+    // pagination
+    // function pagination() {
+    //     let pageNum = pageData.length;
+    //     let paginationTemplate = '';
+
+    //     if(pageNum >= 2) {
+    //         paginationTemplate += '<li><a href="#">&lt; prev</a></li>';
+
+    //         for(let i=0; i<pageNum; i++) {
+    //             paginationTemplate += `
+    //                 <li><a href="#">${i+1}</a></li>
+    //             `
+    //         }
+
+    //         paginationTemplate += '<li><a href="#">next &gt;</a></li>';
+    //     }
+    //     pageRendering.innerHTML = paginationTemplate;
+    // }
+
+    // function changePage(e) {
+    //     e.preventDefault();
+    //     showData.innerHTML = '';
+    //     let num = Number(e.target.textContent);
+    //     console.log(num);
+
+    //     rendering(pageData[num-1]);
+        
+    // }
+
 
     // 事件監聽
     areaSelect.addEventListener('change', changeArea);
     showData.addEventListener('click', showModal);
     hotArea.addEventListener('click', hotZone);
     window.addEventListener('scroll', infinteScroll);
+    // pageRendering.addEventListener('click', changePage);
 
 }(window));
